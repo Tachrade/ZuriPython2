830a36b7d2bd5d4e69eea67ac502955ecb775d2e
Tachrade <tachradeprogrammer@gmail.com>
2022-05-28 16:36:00 +0200

    Add existing file

--- main.py
+++ main.py
@@ -8,27 +8,25 @@ class Student:
      self.age = int(age)
      self.tracks = list(tracks)
      self.score = float(score)
-     print(f"Stundents Details:Old Name:{self.name},Old Age:{self.age}, Old Track:{self.tracks}, Old Score:{self.score}")
-
 
 #Change students name, should accept a new name as an argument.
     def change_name(self, new_name):
      self.new_name = new_name
-     print("The Stundent New Name is:",new_name,self.name)
+     print(f"The Stundent Old Name is: {self.name} , New Name is {new_name}")
      
 #Change students' age, should accept a new age as an argument. Should ensure age remains an integer.
     def change_age(self, new_age):
      self.new_age = int(new_age)
-     print("The Stundent New Age is:", new_age)
+     print(f"The Stundent Old Age is: {self.age} , New Agee is {new_age}")
 
 #Add a new item to students tracks, should accept a new track as an argument
     def add_tracks(self, new_tracks):
-      self.new_tracks = new_tracks
-      print("The Stundent New Tracks is:",new_tracks )
+      self.tracks.append(new_tracks)
+      print(f"The Stundent Tracks are: {self.tracks} , New added track is {new_tracks}")
 
 #get_score: Return student’s score
     def get_score(self):
-     print("The Stundent New Tracks is:",self.score)
+     print(f"The Stundent Old Score still remains the same:",self.score )
     
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
